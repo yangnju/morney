57d1d19f196765fdba72cddebde0211b3c41e081
yangnju <yangfeng611@gmail.com>
2021-02-17 12:58:32 +0800

    添加三个组件，并æ·ï

---
+++ src/views/Money.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+  我是Money组件</div>
+</template>
+
+<script lang="ts">
+export default {
+name: "money"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Statistics.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  我是Statistics组件
+</div>
+</template>
+
+<script lang="ts">
+export default {
+name: "Statistics"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
