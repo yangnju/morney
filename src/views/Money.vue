e5868a09d0590db7e8e53a78cfaaea29afb5a2ec
yangnju <yangfeng611@gmail.com>
2021-02-17 19:56:31 +0800

    money 页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,16 +1,66 @@
 <template>
-<div>
-  我是Money组件
-  <Nav/>
-</div>
+  <div class="nav-wrapper">
+    <div class="content">
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+      <p>我是Money组件</p>
+    </div>
+    <Nav/>
+  </div>
 </template>
 
 <script lang="ts">
 export default {
-name: "money"
-}
+  name: 'money'
+};
 </script>
 
 <style lang="scss" scoped>
+.nav-wrapper {
+  background-color: darkgray;
+  display: flex;
+  //纵向排列
+  flex-direction: column;
+  //容器只能有一个屏幕的高度
+  height: 100vh;
+}
 
+.content {
+  background-color: #42b983;
+  //让内容可以滚动，不然高度还是很高，不是一屏
+  overflow: auto;
+  //尽量把所有的高度都给content，nav占剩下的,这是flex的优势，一下就占满了，很方便
+  flex-grow: 1;
+}
 </style>
