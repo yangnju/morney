1fe976e85e5a2544fd1fc4f912bf000a8929c582
yangnju <yangfeng611@gmail.com>
2021-02-17 19:25:38 +0800

    添加404

---
+++ src/components/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  <p>没找到对应页面，请检查网址是否输入正确</p>
+  <router-link to="/">
+    <button>回到首页</button>
+  </router-link>
+</div>
+</template>
+
+<script lang="ts">
+export default {
+name: "NotFound"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
