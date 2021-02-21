874c3bd9e9c9b677a0454cff6255c7b2d7601f2c
yangnju <yangfeng611@gmail.com>
2021-02-21 13:17:25 +0800

    完成HTML和CSS

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -8,12 +8,12 @@
 const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
 try {importAll(require.context('../assets/icons', true, /\.svg$/));} catch (error) {console.log(error);}
 export default {
-  props:['name'],
-  name: "Icon"
-}
+  props: ['name'],
+  name: 'Icon'
+};
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .icon {
   width: 1em; height: 1em;
   vertical-align: -0.15em;
--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
+  <div class="layout-wrapper" :class="classPrefix && `${classPrefix}-wrapper`">
+    <div class="content" :class="classPrefix && `${classPrefix}-content`   ">
       <slot></slot>
     </div>
     <Nav/>
@@ -9,13 +9,13 @@
 
 <script lang="ts">
 export default {
+  props: ['classPrefix'],
   name: 'Layout'
 };
 </script>
 
 <style lang="scss" scoped>
-.nav-wrapper {
-  border: 1px solid red;
+.layout-wrapper {
   display: flex;
   //纵向排列
   flex-direction: column;
@@ -24,7 +24,6 @@ export default {
 }
 
 .content {
-  background-color: #42b983;
   //让内容可以滚动，不然高度还是很高，不是一屏
   overflow: auto;
   //尽量把所有的高度都给content，nav占剩下的,这是flex的优势，一下就占满了，很方便
